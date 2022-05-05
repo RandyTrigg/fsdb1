6d54343298c7a73d077d67141c61c92f28c662b1
Karen Ishimaru <karenmiko@gmail.com>
2022-05-05 06:51:07 -0700

    feature-selfRegAuto batchedsave

---
+++ force-app/main/default/lwc/bffGrantsSiteHeader/bffGrantsSiteHeader.js
@@ -0,0 +1,3 @@
+import { LightningElement } from 'lwc';
+
+export default class BffGrantsSiteHeader extends LightningElement {}
--- force-app/main/default/lwc/bffGrantsSiteHome/bffGrantsSiteHome.js
+++ force-app/main/default/lwc/bffGrantsSiteHome/bffGrantsSiteHome.js
@@ -1,12 +1,14 @@
 import { LightningElement } from 'lwc';
-
 import getProfileSummary from '@salesforce/apex/ProfileController.getProfileSummary';
 import logoResource from '@salesforce/resourceUrl/BFFLogoGrantsSite';
 import { NavigationMixin } from 'lightning/navigation';
+import { handleError } from 'c/lwcUtilities';
+import Id from '@salesforce/user/Id';
 // import getTranslations from '@salesforce/apex/FormPhraseController.getTranslations';
 // import { buildTransByName } from 'c/formsUtilities';
 
 export default class BffGrantsSiteHome extends LightningElement {
+    userId = Id;
     pageHeader;
     pageSubheader;
     prfButtonLabel;
@@ -14,6 +16,8 @@ export default class BffGrantsSiteHome extends LightningElement {
     grantSubHeading;
     grantDescription;
     bffLogo = logoResource;
+    showMenu = false;
+    hasSubmittedPrf;
 
     propTitle;
     formsTitle;
@@ -23,10 +27,22 @@ export default class BffGrantsSiteHome extends LightningElement {
     langTag;
     language;
     dataLoaded = false;
+    message = "Please create a Profile";
+    hoverMessage;
+    // options = ['English', 'Español', 'Français'];
     
-    async connectedCallback() {
+    connectedCallback() {
+        if (this.userId) {
+            this.loadData();
+        }
+    }
+
+    async loadData() {
         try {
             this.profileSummary = JSON.parse(await getProfileSummary());
+            // Create Form Instance after creating Profile. Linked to Profile and bffProfile form.
+            // Otherwise, look for FormInstance related to this Profile and bffProfile Form.
+            // In either case, return with getProfileSummary.
 
             this.pageHeader = 'bff_GrantsSiteLandingWelcome';
             this.pageSubheader = 'Page subheader';
@@ -35,41 +51,70 @@ export default class BffGrantsSiteHome extends LightningElement {
             this.grantSubHeading = 'bff_GrantsSiteLandingSubtitle';
             this.grantDescription = 'bff_GrantsSiteLandingSustainFund'; // Bundle this into one phrase w/ formatting?
             this.language = this.profileSummary.language;
-
+            this.hasSubmittedPrf = this.profileSummary.hasSubmittedPrf;
+            
+            /* This seems problematic - dataloaded is false.
             this.langMap.set('English', 'en');
-            this.langMap.set('Spanish', 'sp');
-            this.langMap.set('French', 'fr');
+            this.langMap.set('Español', 'sp');
+            this.langMap.set('Français', 'fr');
+            this.langTag = this.langMap.get(this.language);*/
+            this.setLangPickerDefault();
 
             this.dataLoaded = true;
         } catch (error) {
-            // handleError(error);
+            handleError(error);
         }
+    }
 
-    handleLanguage(){
-            this.langTag = langMap.get(this.language);
-        }
+    setLangPickerDefault(){
+        const langPicker = this.template.querySelector('[name="langPicker"]');
+        langPicker.selectedIndex = [...langPicker.options].findIndex(option => option.value === this.language);
+    }
 
-        handleLogout(){
-            this[NavigationMixin.Navigate]({
-                type: 'comm__loginPage',
-                attributes: {
-                  actionName: 'logout'
-                }
-              });    
-        }
-        
-        
+    handleLanguagePicker(event){
+        this.language = event.target.value;
+        // this.langTag = this.langMap.get(this.language);
+    }
+
+    get options() {
+        return [
+                 { label: 'English', value: 'English' },
+                 { label: 'Español', value: 'Spanish' },
+                 { label: 'Français', value: 'French' },
+                 { label: 'Português', value: 'Portuguese' }
+               ];
+    }
+     
+    editProfile() {
+        // Navigate to page with FormInstanceId for Profile
+    }
+    
+    handleNewSolApp(event) {
+
     }
 
 
+    handleMenuSelect () {
+        this.showMenu = !this.showMenu;
+    }
 
+    navigateHome() {
+        this[NavigationMixin.Navigate]({
+            type: 'comm__namedPage',
+            attributes: {
+                name: 'Home'
+            }
+        });
+    }
 
-
-
-
-
-
-
+    handleLogout(){
+        this[NavigationMixin.Navigate]({
+            type: 'comm__loginPage',
+            attributes: {
+              actionName: 'logout'
+            }
+          });
+    }
 
 
 }
